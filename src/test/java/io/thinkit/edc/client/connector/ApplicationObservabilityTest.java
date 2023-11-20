349bc57622fd8fac3a0dcb356226890183009bdb
chaima meliani <chaima.meliani@sesame.com.tn>
2023-11-20 14:59:03 +0100

    feat: add ApplicationObservability.checkHealth method

---
+++ src/test/java/io/thinkit/edc/client/connector/ApplicationObservabilityTest.java
@@ -0,0 +1,44 @@
+package io.thinkit.edc.client.connector;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.http.HttpClient;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+class ApplicationObservabilityTest {
+
+    @Container
+    private final ManagementApiContainer prism = new ManagementApiContainer();
+
+    private final HttpClient http = HttpClient.newBuilder().build();
+    private ApplicationObservability applicationObservability;
+
+    @BeforeEach
+    void setUp() {
+        var client = EdcConnectorClient.newBuilder()
+                .httpClient(http)
+                .managementUrl(prism.getUrl())
+                .build();
+        applicationObservability = client.applicationObservability();
+    }
+
+    @Test
+    void should_get_dataplanes() {
+        var result = applicationObservability.checkHealth();
+
+        assertThat(result.isSucceeded()).isTrue();
+        assertThat(result.getContent().isSystemHealthy()).isTrue();
+        assertThat(result.getContent().componentResults()).isNotNull().first().satisfies(componentResult -> {
+            assertThat(componentResult.component()).isNotNull().isEqualTo("string");
+            assertThat(componentResult.isHealthy()).isTrue();
+            assertThat(componentResult.failure()).isNotNull().satisfies(failure -> {
+                assertThat(failure.failureDetail()).isNotNull().isEqualTo("string");
+                assertThat(failure.messages().size()).isGreaterThan(0);
+            });
+        });
+    }
+}
