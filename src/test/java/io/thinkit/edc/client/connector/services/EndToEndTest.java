0a472250d945abe95e63faca9448eff465193a0b
chaimaMeliani <chaimameliani@think-it.io>
2025-06-11 10:05:31 +0100

    test create asset through real connector

---
+++ src/test/java/io/thinkit/edc/client/connector/services/EndToEndTest.java
@@ -0,0 +1,68 @@
+package io.thinkit.edc.client.connector.services;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.thinkit.edc.client.connector.EdcConnectorClient;
+import io.thinkit.edc.client.connector.RealTimeConnectorApiTestBase;
+import io.thinkit.edc.client.connector.model.Asset;
+import java.net.http.HttpClient;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class EndToEndTest extends RealTimeConnectorApiTestBase {
+
+    private final HttpClient http = HttpClient.newBuilder().build();
+    private Assets assets;
+
+    @BeforeEach
+    void setUp() {
+        var client = EdcConnectorClient.newBuilder()
+                .httpClient(http)
+                .managementUrl(getManagementUrl())
+                .build();
+        assets = client.assets();
+    }
+
+    @Nested
+    class Sync {
+
+        @Test
+        void should_create_an_asset() {
+
+            var created = assets.create(shouldCreateAnAssetRequest());
+
+            assertThat(created.isSucceeded()).isTrue();
+            assertThat(created.getContent()).isNotNull();
+        }
+    }
+
+    @Nested
+    class Async {
+
+        @Test
+        void should_create_an_asset_async() {
+
+            var result = assets.createAsync(shouldCreateAnAssetRequest());
+            assertThat(result).succeedsWithin(timeout, TimeUnit.SECONDS).satisfies(created -> {
+                assertThat(created.isSucceeded()).isTrue();
+                assertThat(created.getContent()).isNotNull();
+            });
+        }
+    }
+
+    private Asset shouldCreateAnAssetRequest() {
+        var properties = Map.of("key", Map.of("value", "value"));
+        var privateProperties = Map.of("private-key", Map.of("private-value", "private-value"));
+        var dataAddress = Map.of("type", "data-address-type");
+
+        return Asset.Builder.newInstance()
+                .id("assetId")
+                .properties(properties)
+                .privateProperties(privateProperties)
+                .dataAddress(dataAddress)
+                .build();
+    }
+}
