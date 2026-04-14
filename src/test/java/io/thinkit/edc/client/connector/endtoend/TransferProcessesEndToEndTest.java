f2e1386eb8af5ef022ea92427464e3a74b485d25
chaimaMeliani <chaimameliani@think-it.io>
2026-04-14 09:15:27 +0100

    feat: add e2e test for transfer process

---
+++ src/test/java/io/thinkit/edc/client/connector/endtoend/TransferProcessesEndToEndTest.java
@@ -0,0 +1,182 @@
+package io.thinkit.edc.client.connector.endtoend;
+
+import static io.thinkit.edc.client.connector.EdcConnectorClient.Versions.V3;
+import static io.thinkit.edc.client.connector.EdcConnectorClient.Versions.V4BETA;
+import static io.thinkit.edc.client.connector.utils.Constants.ODRL_NAMESPACE;
+import static java.util.Collections.emptyList;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
+import io.thinkit.edc.client.connector.EdcConnectorClient;
+import io.thinkit.edc.client.connector.RealTimeConnectorApiTestBase;
+import io.thinkit.edc.client.connector.model.*;
+import io.thinkit.edc.client.connector.model.jsonld.*;
+import io.thinkit.edc.client.connector.model.pojo.PojoCatalogRequest;
+import io.thinkit.edc.client.connector.model.pojo.PojoContractRequest;
+import io.thinkit.edc.client.connector.model.pojo.PojoPolicy;
+import io.thinkit.edc.client.connector.services.management.*;
+import java.net.http.HttpClient;
+import java.util.Map;
+import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedClass;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@ParameterizedClass
+@ValueSource(strings = {V3, V4BETA})
+class TransferProcessesEndToEndTest extends RealTimeConnectorApiTestBase {
+
+    private static final String PROVIDER_PROTOCOL_URL = "http://provider-connector:9194/protocol/2025-1";
+    private static final String PROVIDER_PARTICIPANT_ID = "provider";
+    private static final String DSP_PROTOCOL = "dataspace-protocol-http:2025-1";
+
+    private final HttpClient http = HttpClient.newBuilder().build();
+    private final String managementVersion;
+    private EdcConnectorClient providerClient;
+    private EdcConnectorClient consumerClient;
+
+    TransferProcessesEndToEndTest(String managementVersion) {
+        this.managementVersion = managementVersion;
+    }
+
+    @BeforeEach
+    void setUp() {
+        providerClient = EdcConnectorClient.newBuilder()
+                .httpClient(http)
+                .management(getProviderManagementUrl(), managementVersion)
+                .build();
+
+        consumerClient = EdcConnectorClient.newBuilder()
+                .httpClient(http)
+                .management(getConsumerManagementUrl(), managementVersion)
+                .build();
+
+    }
+
+    @Test
+    void should_get_a_transfer_process() {
+        var assetId = prepareProviderCatalog();
+        var offer = findOfferForAsset(assetId);
+        var negotiationId = initiateNegotiation(offer, assetId);
+
+        await().atMost(timeout, SECONDS).untilAsserted(() -> assertThat(
+                        consumerClient.contractNegotiations().getState(negotiationId).getContent())
+                .isEqualTo("FINALIZED"));
+
+        var negotiation = consumerClient.contractNegotiations().get(negotiationId);
+        var contractAgreementId = negotiation.getContent().contractAgreementId();
+
+        var transferProcessId = consumerClient.transferProcesses().create(transferProcessRequest(contractAgreementId));
+
+        assertThat(transferProcessId.isSucceeded()).isTrue();
+        assertThat(transferProcessId.getContent()).isNotNull();
+        var transferProcess = consumerClient.transferProcesses().get(transferProcessId.getContent());
+
+        assertThat(transferProcess.isSucceeded()).isTrue();
+        await().atMost(timeout, SECONDS).untilAsserted(() -> assertThat(consumerClient.transferProcesses()
+                        .getState(transferProcessId.getContent())
+                        .getContent()
+                        .state())
+                .isEqualTo("STARTED"));
+
+    }
+
+    private Policy findOfferForAsset(String assetId) {
+        var catalog = consumerClient.catalogs().request(catalogRequest());
+
+        var dataset = catalog.getContent().datasets().stream()
+                .filter(d -> assetId.equals(d.id()))
+                .findFirst()
+                .orElseThrow();
+
+        assertThat(dataset.hasPolicy()).isNotEmpty().first().isNotNull();
+        return dataset.hasPolicy().get(0);
+    }
+
+    private String initiateNegotiation(Policy offer, String assetId) {
+        var created = consumerClient.contractNegotiations().create(contractRequest(offer, assetId));
+        assertThat(created.isSucceeded()).isTrue();
+        assertThat(created.getContent()).isNotNull();
+        return created.getContent();
+    }
+
+    private String prepareProviderCatalog() {
+        var assetId = "assetId-" + UUID.randomUUID();
+        providerClient.assets().create(JsonLdAsset.Builder.newInstance()
+                .id(assetId)
+                .properties(Map.of("key1", "value1"))
+                .dataAddress(Map.of("type", "HttpData", "baseUrl", "https://jsonplaceholder.typicode.com/users"))
+                .build());
+
+        var policyId = "policyId-" + UUID.randomUUID();
+        providerClient.policyDefinitions().create(JsonLdPolicyDefinition.Builder.newInstance()
+                .id(policyId)
+                .policy(JsonLdPolicy.Builder.newInstance().build())
+                .build());
+
+        providerClient.contractDefinitions().create(JsonLdContractDefinition.Builder.newInstance()
+                .id("contractDefinitionId-" + UUID.randomUUID())
+                .accessPolicyId(policyId)
+                .contractPolicyId(policyId)
+                .assetsSelector(emptyList())
+                .build());
+
+        return assetId;
+    }
+
+    private CatalogRequest catalogRequest() {
+        if (V3.equals(managementVersion)) {
+            return JsonLdCatalogRequest.Builder.newInstance()
+                    .counterPartyAddress(PROVIDER_PROTOCOL_URL)
+                    .counterPartyId(PROVIDER_PARTICIPANT_ID)
+                    .protocol(DSP_PROTOCOL)
+                    .build();
+        } else {
+            return PojoCatalogRequest.Builder.newInstance()
+                    .counterPartyAddress(PROVIDER_PROTOCOL_URL)
+                    .counterPartyId(PROVIDER_PARTICIPANT_ID)
+                    .protocol(DSP_PROTOCOL)
+                    .build();
+        }
+    }
+
+    private ContractRequest contractRequest(Policy offer, String assetId) {
+        if (V3.equals(managementVersion)) {
+            return JsonLdContractRequest.Builder.newInstance()
+                    .counterPartyAddress(PROVIDER_PROTOCOL_URL)
+                    .protocol(DSP_PROTOCOL)
+                    .policy(JsonLdPolicy.Builder.newInstance()
+                            .raw(((JsonLdPolicy) offer).raw())
+                            .type(ODRL_NAMESPACE + "Offer")
+                            .assigner(PROVIDER_PARTICIPANT_ID)
+                            .target(assetId)
+                            .build())
+                    .build();
+        } else {
+            return PojoContractRequest.Builder.newInstance()
+                    .counterPartyAddress(PROVIDER_PROTOCOL_URL)
+                    .protocol(DSP_PROTOCOL)
+                    .policy(PojoPolicy.Builder.newInstance()
+                            .id(offer.id())
+                            .type(ODRL_NAMESPACE + "Offer")
+                            .assigner(PROVIDER_PARTICIPANT_ID)
+                            .target(assetId)
+                            .build())
+                    .build();
+        }
+    }
+
+    private TransferRequest transferProcessRequest(String contractAgreementId) {
+        var dataDestination = Map.of("type", "HttpProxy");
+        return TransferRequest.Builder.newInstance()
+                .protocol(DSP_PROTOCOL)
+                .counterPartyAddress(PROVIDER_PROTOCOL_URL)
+                .connectorId(PROVIDER_PARTICIPANT_ID)
+                .contractId(contractAgreementId)
+                .transferType("HttpData-PULL")
+                .dataDestination(dataDestination)
+                .build();
+    }
+}
